fa3f642d5bc37b3d4bce5f1d9f35d50cf8765fdb
Saurabh Jadhav <36992945+jad837@users.noreply.github.com>
2024-12-22 21:56:00 +0530

    Update util.go

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,12 +1,18 @@
 package raft
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 // Debugging
-const Debug = false
+const Debug = true
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
+		log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+		format = fmt.Sprintf("[PID %d] %s", os.Getpid(), format)
 		log.Printf(format, a...)
 	}
 	return
