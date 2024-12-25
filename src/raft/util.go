bec35d51cd2fdeb2a141a0513fd4e48397c0a04e
jad837 <jadhavsaurabh037@gmail.com>
2024-12-25 20:18:42 +0530

    improvised logging for debugging

--- src/raft/util.go
+++ src/raft/util.go
@@ -4,15 +4,74 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"time"
 )
+
+func getVerbosity() int {
+	v := os.Getenv("VERBOSE")
+	level := 0
+	if v != "" {
+		var err error
+		level, err = strconv.Atoi(v)
+		if err != nil {
+			log.Fatalf("Invalid verbosity level: %s", v)
+		}
+	}
+	return level
+}
+
+type logTopic string
+
+const (
+	dClient  logTopic = "CLNT"
+	dCommit  logTopic = "CMIT"
+	dDrop    logTopic = "DROP"
+	dError   logTopic = "ERRO"
+	dInfo    logTopic = "INFO"
+	dLeader  logTopic = "LEAD"
+	dLog     logTopic = "LOG1"
+	dLog2    logTopic = "LOG2"
+	dPersist logTopic = "PERS"
+	dSnap    logTopic = "SNAP"
+	dTerm    logTopic = "TERM"
+	dTest    logTopic = "TEST"
+	dTimer   logTopic = "TIMR"
+	dTrace   logTopic = "TRCE"
+	dVote    logTopic = "VOTE"
+	dWarn    logTopic = "WARN"
+)
+
+var debugVerbosity int
+var debugStart time.Time
 
 // Debugging
 const Debug = false
+const Info = true
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug {
-		log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-		format = fmt.Sprintf("[PID %d] %s", os.Getpid(), format)
+func init() {
+	debugVerbosity = getVerbosity()
+	debugStart = time.Now()
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+}
+
+func DPrintf(topic logTopic, format string, a ...interface{}) (n int, err error) {
+	if debugVerbosity > 0 {
+		time := time.Since(debugStart).Microseconds()
+		time /= 100
+		prefix := fmt.Sprintf("%06d [DEBUG] %v ", time, string(topic))
+		format = prefix + format
+		log.Printf(format, a...)
+	}
+	return
+}
+
+func IPrintf(topic logTopic, format string, a ...interface{}) (n int, err error) {
+	if Info {
+		time := time.Since(debugStart).Microseconds()
+		time /= 100
+		prefix := fmt.Sprintf("%06d [INFO] %v ", time, string(topic))
+		format = prefix + format
 		log.Printf(format, a...)
 	}
 	return
